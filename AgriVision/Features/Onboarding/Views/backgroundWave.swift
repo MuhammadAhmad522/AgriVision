046d73d03a548f8737f8041872e0ec13e2d3e2d2
Muhammad Ahmad <muhammadahmad522@gmail.com>
2026-03-02 13:55:55 +0500

    feat: add onboarding screens, associated image assets, and a new centralized color palette.

---
+++ AgriVision/Features/Onboarding/Views/backgroundWave.swift
@@ -0,0 +1,18 @@
+//
+//  backgroundWave.swift
+//  AgriVision
+//
+//  Created by Muhammad Ahmad on 28/02/2026.
+//
+
+import SwiftUI
+
+struct backgroundWave: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    backgroundWave()
+}
